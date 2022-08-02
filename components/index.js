92df7fb887b72ebaa482ec89e8cd8f6b7d1e85bd
anshen <plouisin@gmail.com>
2022-08-02 20:56:06 +0200

    create the news section of the widgets component

--- components/index.js
+++ components/index.js
@@ -4,3 +4,4 @@ export { default as Feed } from './Feed';
 export { default as Input } from './Input';
 export { default as Post } from './Post';
 export { default as Widgets } from './Widgets';
+export { default as News } from './News';
--- readme.txt
+++ readme.txt
@@ -1,6 +1,10 @@
 Finish: https://twitter.100jsprojects.com/
 
 Reprendre à 2h35mn
+2h52:40
 
 
 https://www.youtube.com/watch?v=PnvDPSg5bQM
+
+API WITHOUY KEY UPDATE
+https://saurav.tech/NewsAPI/
